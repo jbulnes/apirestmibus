028ecd239aabcb0d316baa8222fc230bf4786738
Junior Javier Sanchez <juniorsc750@gmail.com>
2025-06-12 15:31:19 -0500

    Update repository

--- src/main/java/com/ApiRestMiBus/model/service/UserDetailServiceImpl.java
+++ src/main/java/com/ApiRestMiBus/model/service/UserDetailServiceImpl.java
@@ -86,6 +86,9 @@ public class UserDetailServiceImpl implements UserDetailsService {
             throw new BadCredentialsException("Invalid Username or password");
         }
 
+        System.out.println("Contraseña ingresada: " + password);
+        System.out.println("Contraseña en BD: " + userDetails.getPassword());
+
         if(!passwordEncoder.matches(password, userDetails.getPassword())){
             throw new BadCredentialsException("Invalid password");
         }
